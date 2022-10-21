7a548e9520520f255854093e9e8801ec75b6d02a
sihyun-mity <kailker4467@gmail.com>
2022-10-21 18:22:08 +0900

    검색 결과 출력

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,18 +1,40 @@
 import axios from 'axios';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
-import { useLocation } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { useMountEffect, usePathQuery } from '../../hooks';
-import { headerSearchBar } from '../../store';
+import { headerSearchBar, orientationType, responsiveType } from '../../store';
 import SearchResponseDataModel from './types/SearchResponseDataModel';
 
 const Search = (): JSX.Element => {
   const qs = usePathQuery();
-  const { pathname } = useLocation();
   const setHeaderSearchBar = useSetRecoilState(headerSearchBar);
-  const refetch = useRef<boolean>(false);
+  const deviceResponsive = useRecoilValue(responsiveType);
+  const deviceOrientation = useRecoilValue(orientationType);
+  const [itemsLine, setItemsLine] = useState<number[]>();
+
+  const calculateItemsLine = useCallback((): void => {
+    let count: number;
+
+    switch (deviceResponsive) {
+      case 'mobile':
+        count = 2;
+        break;
+
+      case 'tablet':
+        count = 3;
+        break;
+
+      case 'pc':
+        count = 4;
+        break;
+    }
+
+    if (deviceOrientation === 'landscape') count *= 2;
+
+    setItemsLine(new Array(count).fill(1));
+  }, [deviceResponsive, deviceOrientation]);
 
   const fetchData = async (): Promise<void> => {
     const headers = { 'Ocp-Apim-Subscription-Key': process.env.REACT_APP_API_KEY };
@@ -22,63 +44,58 @@ const Search = (): JSX.Element => {
     return data;
   };
 
-  const { data }: { data?: SearchResponseDataModel } = useQuery([`searchData`, refetch.current], fetchData, {
+  const { data }: { data?: SearchResponseDataModel } = useQuery(['searchData', qs['keyword']], fetchData, {
     staleTime: Infinity,
     suspense: true,
   });
 
   const { value: images } = { ...data };
 
+  useEffect(() => calculateItemsLine(), [calculateItemsLine]);
+
   useMountEffect(() => setHeaderSearchBar(true));
 
-  useEffect(() => {
-    refetch.current = true;
-  }, [pathname]);
-
   return (
-    <Box>
-      <Items>
-        {images?.map(
-          (item, index) =>
-            index % 2 !== 0 && (
-              <Item key={index}>
-                <Image src={item.thumbnailUrl} />
-                <Name>{item.name}</Name>
-              </Item>
-            ),
-        )}
-      </Items>
-      <Items>
-        {images?.map(
-          (item, index) =>
-            index % 2 === 0 && (
-              <Item key={index}>
-                <Image src={item.thumbnailUrl} />
-                <Name>{item.name}</Name>
-              </Item>
-            ),
-        )}
-      </Items>
+    <Box count={itemsLine?.length || 0}>
+      {itemsLine?.map((item, index) => (
+        <Items key={index}>
+          {(() => {
+            let startIdx: number = index;
+
+            return images?.map((item, index) => {
+              const findIdx: number = startIdx;
+              startIdx += itemsLine.length;
+
+              return (
+                images[findIdx] && (
+                  <Item key={index}>
+                    <Image src={images[findIdx].thumbnailUrl} />
+                    <Name>{images[findIdx].name}</Name>
+                  </Item>
+                )
+              );
+            });
+          })()}
+        </Items>
+      ))}
     </Box>
   );
 };
 
 export default Search;
 
-const Box = styled.article`
+const Box = styled.article<{ count: number }>`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: flex-start;
   margin-top: 16px;
-`;
 
-const Items = styled.div`
-  width: calc(50% - 10px);
-
-  &:nth-child(odd) {
-    margin-right: 20px;
+  & > div {
+    width: calc(100% / ${(props) => props.count} - 10px);
   }
 `;
+
+const Items = styled.div``;
 
 const Item = styled.div`
   width: 100%;
